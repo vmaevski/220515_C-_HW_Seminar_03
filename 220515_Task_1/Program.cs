a714bbd3130c6612a77cbbf33603adb58222ed58
vmaevski <vmaevski@mail.ru>
2022-05-16 07:27:46 +0700

    Task 1 completed (method 1). I keep thinking about method 2...

--- 220515_Task_1/Program.cs
+++ 220515_Task_1/Program.cs
@@ -1,12 +1,19 @@
 ﻿// 1. Напишите программу, которая принимает на вход пятизначное число и 
 // проверяет, является ли оно полиндромом ("зеркальным"). 
 
-void Polindrom(num)
+void Polindrom_1(int num)
 {
-    
+    string Text = ($"NO. The number {num} is not a Polydrom.");
+    string str = num.ToString();
+    if (str[0] == str[4] && str[1] == str[3])
+    {
+        Text = ($"Yes. The number {num} is the Polydrome.");
+    }
+    Console.WriteLine(Text);
 }
 
+void 
 
-Polindrom(12412);
-Polindrom(23432);
-Polindrom(12821);
+Polindrom_1(12412);
+Polindrom_1(23432);
+Polindrom_1(12821);
