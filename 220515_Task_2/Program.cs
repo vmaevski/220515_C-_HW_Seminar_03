b9530c6840b8409dd273d497af8e13158d628f03
vmaevski <vmaevski@mail.ru>
2022-05-17 08:14:49 +0700

    Task 2 completed.

---
+++ 220515_Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве.
+
+double DistAB(double xa, double ya, double za, double xb, double yb, double zb)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2) + Math.Pow(za - zb, 2)), 2);
+}
+
+
+double XA = 3; double YA = 6; double ZA = 0;
+double XB = 2; double YB = 1; double ZB = 0;
+Console.WriteLine($"The distance between points"); 
+Console.WriteLine($"A (coordinates: x = {XA}, y = {YA}, z = {ZA}) and");
+Console.WriteLine($"B (coordinates: x = {XB}, y = {YB}, z = {ZB})");
+Console.WriteLine($"= {DistAB(XA, YA, ZA, XB, YB, ZB)}.");
+
